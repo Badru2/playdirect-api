4ba07cd7cc080b0df4875faf363ca22be0284d38
Badru2 <hilalbadru4@gmail.com>
2024-11-13 19:02:48 +0700

    feat: update cart and in adding cart if its exist then update

--- controllers/cart-controller.js
+++ controllers/cart-controller.js
@@ -4,11 +4,20 @@ const CreateCart = async (req, res) => {
   const { user_id, product_id, quantity } = req.body;
 
   try {
-    const cart = await Cart.create({
-      user_id,
-      product_id,
-      quantity,
+    const cart = await Cart.findOne({
+      where: { user_id, product_id },
     });
+
+    if (cart) {
+      cart.quantity += quantity;
+      await cart.save();
+    } else {
+      cart = await Cart.create({
+        user_id,
+        product_id,
+        quantity,
+      });
+    }
 
     res.status(201).json({
       success: true,
@@ -44,6 +53,34 @@ const GetCarts = async (req, res) => {
   }
 };
 
+const UpdateCart = async (req, res) => {
+  const { product_id, action } = req.body;
+
+  try {
+    const cart = await Cart.findOne({
+      where: { product_id },
+    });
+
+    if (action === "increment") {
+      cart.quantity += 1;
+    } else if (action === "decrement") {
+      cart.quantity -= 1;
+    }
+
+    await cart.save();
+
+    res.status(201).json({
+      success: true,
+      data: cart,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
 const DeleteCart = async (req, res) => {
   const { id } = req.params;
 
@@ -69,5 +106,6 @@ const DeleteCart = async (req, res) => {
 module.exports = {
   CreateCart,
   GetCarts,
+  UpdateCart,
   DeleteCart,
 };
--- routes/cart-router.js
+++ routes/cart-router.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { GetCarts, CreateCart, DeleteCart } = require("../controllers/cart-controller");
+const { GetCarts, CreateCart, DeleteCart, UpdateCart } = require("../controllers/cart-controller");
 
 const router = express.Router();
 
 router.get("/", GetCarts);
 router.post("/store", CreateCart);
+router.put("/update/:id", UpdateCart);
 router.delete("/destroy/:id", DeleteCart);
 
 module.exports = router;
